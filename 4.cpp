415a20f723e9a34edff6ee2bd8c829efd6832f1b
BSKorolev <90824350+BSKorolev@users.noreply.github.com>
2021-12-16 13:02:25 +0200

    add4

---
+++ 4.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+	int s = 0;
+	for (int i = 0; i <= 999; i++)
+		for (int j = 0; j <= 999; j++)
+			if (i / 100 + i % 100 / 10 + i % 10 == j / 100 + j % 100 / 10 + j % 10)
+				s += 1;
+	std::cout << s;
+}
