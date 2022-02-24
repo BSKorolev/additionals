fa565890dc0987b40ad790e8b1fa9738f027ce3d
BSKorolev <90824350+BSKorolev@users.noreply.github.com>
2022-02-24 12:52:39 +0200

    Update main.cpp

--- 2.2/main.cpp
+++ 2.2/main.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 
-void Countsort(int n, int mas[]) 
+void Countsort(int n, int mas[])
 {
-    int m = 0;
-    int M = 32768;
-    int c[32768];
+
+    int m = INT_MAX;
+    int M = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (mas[i] > M)
+            M = mas[i];
+        if (mas[i] < m)
+            m = mas[i];
+    }
+    int a;
+    a = M - m + 1;
+    int* c = new int[a];
     int y[10];
-    for (int i = m; i < M; i++)
+    for (int i = 0; i < a; i++)
         c[i] = 0;
     for (int j = 0; j < n; j++)
     {
-        int k = mas[j];
+        int k = mas[j]-m;
         c[k]++;
     }
-    for (int i = m + 1; i < M; i++)
+    for (int i =1; i < a; i++)
     {
         c[i] += c[i - 1];
     }
-    for (int j = 0; j < n; j++)
+    for (int j = 0; j <n; j++)
     {
-        int k = mas[j];
+        int k = mas[j]-m;
         y[c[k] - 1] = mas[j];
         c[k]--;
     }
     for (int j = 0; j < n; j++)
         mas[j] = y[j];
+    delete[] c;
 }
 int main()
 {
@@ -32,8 +43,8 @@ int main()
     std::cin >> n;
     int* mas = new int[n];
     for (int i = 0; i < n; i++)
-        mas[i]=rand();
-    Countsort(n,mas);
+        std::cin >> mas[i];
+    Countsort(n, mas);
     for (int i = 0; i < n; i++)
         std::cout << mas[i] << " ";
     delete[] mas;
